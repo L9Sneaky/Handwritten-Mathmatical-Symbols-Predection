20d082325004819478240388236220eee731f938
l9sneaky <pspsony200888@gmail.com>
2022-10-19 10:23:56 +0300

    added model

--- handwrittin/hwapp/views.py
+++ handwrittin/hwapp/views.py
@@ -9,7 +9,6 @@ import numpy as np
 import cv2
 import yaml
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 # Create your views here.
 
 
